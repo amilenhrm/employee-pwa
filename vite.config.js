7870a999eba9ec3deba866e9b34b5bbc01d9d69d
AMILEN <amilenhrm@gmail.com>
2025-09-26 13:17:22 +0800

    Fix vite config and Node.js version

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue' // if using Vue
 
 export default defineConfig({
-  base: '/employee-pwa/', // <- this is important
-  plugins: [vue()],
-})
-export default defineConfig({
   plugins: [react()],
   base: "/employee-pwa/", // replace with your repo name
 })
